2e0997556b808cfa8113f348996ee845230302b0
Mustafa <109014686+mustafaryk@users.noreply.github.com>
2025-09-21 16:49:13 -0400

    the program is a true peer 2 peer experience, you can connect disconnect middle of execution

--- peer.c
+++ peer.c
@@ -27,17 +27,17 @@ void disconnect(){
 	FD_CLR(cfd, &all_sockets);
 	close(cfd);
 	cfd = -1;
+	printf("Waiting for connection as a server now, if you would like to connect type: \"CONNECT:IP_ADDRESS PORT_NUMBER\"\n");
 }
 
 void connect_as_server(){		//we as a peer are connecting as a server (waiting for a client)
 	struct sockaddr_in ca;
 	socklen_t sinlen = sizeof(struct sockaddr_in);
-	printf("Now waiting for connnection as a server.\n");
 	cfd = accept(sfd, (struct sockaddr *)&ca, &sinlen);				//this is blocking, will wait untill a client comes to connect
 	FD_SET(cfd, &all_sockets);
 	char dot_notation[INET_ADDRSTRLEN];
 	inet_ntop(AF_INET, &(&ca)->sin_addr, dot_notation, INET_ADDRSTRLEN);
-	printf("Success in connecting to client with IP address: %s and with port number: %d\n", dot_notation, ntohs(ca.sin_port));
+	printf("Success in connecting to client with IP address: %s and with port number: %d, if you would like to disconnect type: \"DISCONNECT\"\n\n", dot_notation, ntohs(ca.sin_port));
 	return;
 }
 
@@ -58,19 +58,36 @@ void connect_as_client(char* ip_address,int port_number){
 		disconnect();
 		return;
 	}
-	printf("Success in connecting to server with IP address: %s and with port number: %d\n", ip_address, port_number);
+	printf("Success in connecting to server with IP address: %s and with port number: %d, if you would like to disconnect type: \"DISCONNECT\"\n\n", ip_address, port_number);
 	FD_SET(cfd, &all_sockets);
 	
 }
 
 void write_to_client(){           //helper function to write to our client  
-    char message[1024];  
+    char message[1024];
     if (fgets(message, sizeof(message), stdin) == NULL){     	//read message from standard input
 		return;
-    }  
+    }
+	if (strcmp(message, "DISCONNECT\n") == 0){
+		disconnect();
+		return;
+	}
 	encrypt(message);
     if (write(cfd, message, strlen(message)) <= 0){			//client has stopped reading
 		disconnect();
+	}
+}
+
+void handle_input(){
+	char message[1024];
+	char ip_address[32];
+	char port_number[32];
+    if (fgets(message, sizeof(message), stdin) == NULL){     	//read message from standard input
+		return;
+    }
+	if (sscanf(message, "CONNECT:%s %s", ip_address, port_number) == 2){
+		connect_as_client(ip_address, atoi(port_number));
+		return;
 	}
 }
 
@@ -139,11 +156,11 @@ int main(int argc, char **argv){
 		connect_as_client(argv[2] , atoi(argv[3]));		//second argument is peer ip, third argument is peers port number
 	}
 	
-    for(;;){		//loop
+	if (cfd == -1){
+		printf("Waiting for connection as a server now, if you would like to connect type: \"CONNECT:IP_ADDRESS PORT_NUMBER\" and if you would like to disconnect type: \"DISCONNECT\"\n");
+	}
 	
-		if (cfd == -1){				//we dont have a peer
-			connect_as_server();
-		}
+    for(;;){		//loop
 	
 		ready_read_sockets = all_sockets;
 		ready_write_sockets = all_sockets;
@@ -153,11 +170,19 @@ int main(int argc, char **argv){
             return -1;  
         }
 		
+		if (FD_ISSET(sfd, &ready_read_sockets) && cfd == -1){
+			connect_as_server();
+		}
+		
 		if (FD_ISSET(cfd, &ready_read_sockets)){				//peer has sent data
 			handle_client_data();
 		}
 		
-		if (FD_ISSET(STDIN_FILENO,  &ready_read_sockets) && cfd !=-1){		//we have data ready to send to peer
+		if (FD_ISSET(STDIN_FILENO,  &ready_read_sockets) && cfd == -1){		//we have some input for user
+			handle_input();
+		}
+		
+		if (FD_ISSET(STDIN_FILENO,  &ready_read_sockets) && cfd != -1){		//we have data ready to send to peer
 			write_to_client();
 		}
 	}
